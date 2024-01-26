3ec4b3e88b85a42ad4737ae497394eb9482ccfaa
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-01-26 10:41:26 -0300

    .

--- src/galerias/Sopao.tsx
+++ src/galerias/Sopao.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, Text, StyleSheet, Image, FlatList } from 'react-native';
 import { TextosInicio } from '../mocks/textos';
 
 const Sopao: React.FC = () => {
+  const imagens = [
+    require('../../assets/Imagens/sopao/imagem1.png'),
+    require('../../assets/Imagens/sopao/imagem2.png'),
+    require('../../assets/Imagens/sopao/imagem3.png'),
+    require('../../assets/Imagens/sopao/imagem4.png'),
+    require('../../assets/Imagens/sopao/imagem5.png'),
+
+  ];
+
+  const renderItem = ({ item }: { item: any }) => {
+    return <Image source={item} style={styles.image} />;
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.titulo}>{TextosInicio.tituloCard2}</Text>
 
       <Text style={styles.descricaoProjeto}>Num canto remoto do universo, onde as estrelas dançam em harmonia, uma pequena nave espacial navegava pelo cosmos, explorando os segredos do espaço infinito. A tripulação, composta por seres de diferentes galáxias, compartilhava histórias fascinantes sobre os mundos que haviam descoberto.</Text>
-    </View>
+
+      <FlatList
+        data={imagens}
+        renderItem={renderItem}
+        keyExtractor={(_, index) => index.toString()}
+        horizontal={false}
+        numColumns={1}
+      />
+    </ScrollView>
   );
 };
 
@@ -18,16 +39,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 16,
     marginVertical: 10,
+    marginBottom: 20
   },
   titulo: {
     fontSize: 28,
     fontWeight: 'bold',
     marginBottom: 16,
-    textAlign: "center",
+    textAlign: 'center',
   },
-  descricaoProjeto:{
-    color: "#3C533C",
-    fontSize: 19
+  descricaoProjeto: {
+    color: '#3C533C',
+    fontSize: 19,
+    marginBottom: 20,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'cover',
+    marginBottom: 10,
+
   },
 });
 
